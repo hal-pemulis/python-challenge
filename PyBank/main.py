4560982964f7511687907da5d08d0797d0088472
hal-pemulis <33302492+hal-pemulis@users.noreply.github.com>
2020-02-14 13:32:07 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,21 +19,15 @@ with open('budget_data.csv', 'r') as csvfile:
         net_total += int(row[1])
         months.append(row[0])
         profit_loss.append(int(row[1]))
-        #print(row)
 
-    # print(months)
-    # print(profit_loss[0]+profit_loss[0])
-
-    for i in profit_loss)-1):
-        print(profit_loss[i+1] - profit_loss[i])
-
+    for i in range(len(profit_loss)):
+        if i > 0:
+            average_change.append(profit_loss[i] - profit_loss[i-1])
 
     print(f'Total Months: {total_months}')
     print(f'Total: {net_total}')
-    print(f'Average Change: {sum(profit_loss)/len(profit_loss)}')
+    print(f'Average Change: {int(sum(average_change)/len(average_change))}')
     print(f'Greatest Increase in Profits: {months[profit_loss.index(max(profit_loss))]} ({max(profit_loss)})')
     print(f'Greatest Decrease in Profits: {months[profit_loss.index(min(profit_loss))]} ({min(profit_loss)})')
 
 csvfile.close()
- 
-
