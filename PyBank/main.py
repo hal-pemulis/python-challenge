f4b2373519267d0867a0075ff51d462346cfac41
hal-pemulis <33302492+hal-pemulis@users.noreply.github.com>
2020-02-15 11:08:47 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,10 @@
 import os
 import csv
 
-with open('budget_data.csv', 'r') as csvfile:
+csvpath = os.path.join('..', 'CSV_files', 'budget_data.csv')
+
+
+with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
 
     # Skip header
@@ -21,14 +24,27 @@ with open('budget_data.csv', 'r') as csvfile:
         months.append(row[0])
         profit_loss.append(int(row[1]))
 
-    for i in range(len(profit_loss)):
-        if i > 0:
-            average_change.append(profit_loss[i] - profit_loss[i-1])
+    csvfile.close()
 
-    print(f'Total Months: {total_months}')
-    print(f'Total: {net_total}')
-    print(f'Average Change: {int(sum(average_change)/len(average_change))}')
-    print(f'Greatest Increase in Profits: {months[profit_loss.index(max(profit_loss))]} ({max(profit_loss)})')
-    print(f'Greatest Decrease in Profits: {months[profit_loss.index(min(profit_loss))]} ({min(profit_loss)})')
+for i in range(len(profit_loss)):
+    if i > 0:
+        average_change.append(profit_loss[i] - profit_loss[i-1])
 
-csvfile.close()
+print(f'Total Months: {total_months}')
+print(f'Total: {net_total}')
+print(f'Average Change: {int(sum(average_change)/len(average_change))}')
+print(f'Greatest Increase in Profits: {months[profit_loss.index(max(profit_loss))]} ({max(profit_loss)})')
+print(f'Greatest Decrease in Profits: {months[profit_loss.index(min(profit_loss))]} ({min(profit_loss)})')
+
+
+txtpath = os.path.join('..', 'CSV_files', 'budget_data.txt')
+
+with open(txtpath, 'w') as txtfile:
+
+    txtfile.write(f'Total Months: {total_months}\r')
+    txtfile.write(f'Total: {net_total}\r')
+    txtfile.write(f'Average Change: {int(sum(average_change)/len(average_change))}\r')
+    txtfile.write(f'Greatest Increase in Profits: {months[profit_loss.index(max(profit_loss))]} ({max(profit_loss)})\r')
+    txtfile.write(f'Greatest Decrease in Profits: {months[profit_loss.index(min(profit_loss))]} ({min(profit_loss)})\r')
+
+    txtfile.close()
