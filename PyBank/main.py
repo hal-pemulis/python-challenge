4b486701404845d48b715de92a10955b62309a4b
hal-pemulis <33302492+hal-pemulis@users.noreply.github.com>
2020-02-17 16:16:52 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,48 @@
 #PyBank
 
+# import modules
 import os
 import csv
 
+# set variable to file path
 csvpath = os.path.join('..', 'CSV_files', 'budget_data.csv')
 
-
+# access file and begin loop
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
 
-    # Skip header
+    # Skip/store header
     csv_header = next(csvreader)
-
+    
+    # set variables to 0 and empty lists
     total_months = 0
     net_total = 0
     months = []
     profit_loss = []
     average_change = []
 
+    # begin loop through CSV rows
     for row in csvreader:
+        
+        # for every row, add 1 to 'total months'
         total_months += 1
+        
+        # add every row's 'profit/loss' to 'net_total' variable
         net_total += int(row[1])
+        
+        # serperate months and profit/loss into different lists 
+        # by appending as the program loops through the rows
+        # ('zip' may save space here and be more efficient, but I thought this was more intuitive)
         months.append(row[0])
         profit_loss.append(int(row[1]))
 
+    # done with CSV file
     csvfile.close()
 
+# begin loop through 'profit_loss' list to create list of changes
 for i in range(len(profit_loss)):
+    
+    # to find 
     if i > 0:
         average_change.append(profit_loss[i] - profit_loss[i-1])
 
